48fec91be63b3df36085d3af8754e83b416ccf18
lf17708152275@gamil.com <lf17708152275@gamil.com>
2026-01-13 16:29:37 +0800

    更新子模块 hbb_common 的 URL 和提交 ID，调整 API 服务器的默认值获取方式

--- src/common.rs
+++ src/common.rs
@@ -1085,7 +1085,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    match option_env!("API_SERVERS") { Some(s) => s.to_owned(), None => "https://admin.rustdesk.com".to_owned(), }
 }
 
 #[inline]
